701f4e2d3b7956d835495e0d656382656f78d66f
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-13 14:29:19 +0530

    1st commit

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -5,5 +5,18 @@
 # or if x is greater than the upper bound, return the upper bound.
 
 def fun_getinrange(x, bound1, bound2):
-	# your code goes here
-	return -1
+    if(bound1<bound2):
+        if (x<bound1):
+            return bound1
+        elif x>bound2:
+            return bound2
+        else:
+            return x
+    if(bound2<bound1):
+         if (x<bound2):
+                return bound2
+         elif x>bound1:
+            return bound1
+         else:
+            return x
+    
