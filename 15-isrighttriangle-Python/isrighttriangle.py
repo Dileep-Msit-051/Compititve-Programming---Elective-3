649c4c9b759c37bc6bc30430703716671bce29fd
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-15 17:01:25 +0530

    1

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -6,5 +6,13 @@
 # almostEqual (instead of ==) when comparing floats.
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
+    p= ((x1-x2)**2)+((y1-y2)**2)
+    q= ((x1-x3)**2)+((y1-y3)**2)
+    r= ((x2-x3)**2)+((y2-y3)**2)
+    
+    if((p > 0 and q > 0 and r > 0) and (p == (q+r) or q == (p+r) or r == (p+q))):
+        return True
+    else:
+        return False 
 	# your code goes here
-	pass
+	
