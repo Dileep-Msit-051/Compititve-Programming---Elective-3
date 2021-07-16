3873c41f365ab241d6f1b3ababb0c94a8c48fde5
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-16 14:58:42 +0530

    1

--- 02-queues-Java/Queue.java
+++ 02-queues-Java/Queue.java
@@ -1,22 +1,72 @@
 public class Queue{
-	int[] storage = new int[20];
-	int front;
-	int rear;
-	public Queue(int head){
-		front = 0;
-		rear = 0;
-		storage[rear++] = head;
-	}
-	
-	public void enqueue(int new_ele){
+    int queue[]=new int[5];
+    int size;
+    int front;
+    int rear;
 
-	}
+    public void enqueue(int data){
+        if(!isFull()){
+        queue[rear]=data;
+        rear=(rear+1)%5;
+        size=size+1;
+        }
+        else{
+            System.out.println("queue if Full");
+        }
 
-	public int peek(){
-		return 0;
-	}
+    }
+    public int dequeue()
+    {
+      int data=queue[front];
+      if(!isEmpty()){
+      front=(front+1)%5;
+      size=size-1;
+      }
+      else{
+          System.out.println("queue is Empty");
+      }
+      return data;
 
-	public int dequeue(){
-		return 0;
-	}
+
+    }
+    public void show(){
+        System.out.print("Elements: ");
+        for(int i=0;i<size;i++){
+            System.out.print(queue[(front+i)%5]+" ");
+        }
+        System.out.println();
+        for(int n: queue){
+            System.out.print(n +" ");
+        }
+    }
+    public int getsize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean isFull(){
+        return size==5;
+    }
+
+    public static void main(String[] args){
+        Queue q=new Queue();
+        q.enqueue(5);
+        q.enqueue(2);
+        q.enqueue(7);
+        q.enqueue(3);
+       
+        q.dequeue();
+        q.dequeue();
+       
+        q.enqueue(1);
+        q.enqueue(9);
+
+        System.out.println(q.isEmpty());
+        System.out.println(q.isFull());
+        
+        System.out.println("size"+q.getsize());
+        q.show();
+
+    }
 }
