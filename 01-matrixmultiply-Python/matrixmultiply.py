c6cad4889ee8773efc82b348c2fed2bb33ae6bac
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-21 21:41:27 +0530

    1

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,8 +5,20 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
-
-
-
-
+    if(len(m1[0])!=len(m2)):
+        return None
+    arr=[]
+    for i in range(0,len(m1)):
+        #print (arr)
+        arr.append([])
+        #print
+    for i in range (0,len(m1)):
+        for j in range (0,len(m2[0])):
+            arr[i].append(j)
+            #print(arr)           
+            arr[i][j]=0
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                arr[i][j]+=m1[i][k]*m2[k][j]
+    return arr
