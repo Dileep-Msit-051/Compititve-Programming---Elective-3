e89c971aca221eee56c33b3919401e0260a13f35
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-23 10:33:05 +0530

    1

--- 01-linkedlist-Java/LinkedList.java
+++ 01-linkedlist-Java/LinkedList.java
@@ -13,9 +13,33 @@ public class LinkedList{
 	public LinkedList(Element head){
 		this.head = head;
 	}
+ public void display() {
+        LinkedList current = head;
+        while (current != null) {
+            System.out.print(current.value + "-- ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
 
-	public void append(Element new_element){
-		// Your code goes here
+ public int length() {
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+        Element current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+public void append(Element new_element){
+		LinkedList newnode= new LinkedList(new_element);
+		newnode.next=head;
+		head=newnode;
+
 	}
 
 	public Element get_position(int position){
