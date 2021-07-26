49a9c59bdb2e7a417292d39ede5dd99bb35a3eca
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-07-26 12:34:43 +0530

    1

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -10,6 +10,27 @@ elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
+
+
+def recursionOnlyBinarySearchValues(L,v):
+    #print(L,v)
+    return binarysearch(L,v,0,len(L)-1)
+    
+def binarysearch(L,v,low,high,res=[]):
+    mid=(low+high)//2 #0,1
+    if (L[mid] == v and low<high):#acfgmq #a
+        res.append((mid,L[mid]))
+        return res
+    elif(low == high):#1,1
+        res.append((mid,L[mid]))
+        return res
+    elif(v<L[mid]):#a<f
+        high=mid-1 #1 low 0
+        res.append((mid,L[mid]))#mid=0
+    else:
+        low=mid+1
+        res.append((mid,L[mid]))
+    return binarysearch(L,v,low,high,res)
+
 def binary_search(input_array, value):
-    # Your code goes here
-    pass
+
