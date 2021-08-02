7513977714bdffc4bc6ac90e19e3eea354ca84f2
Dileep-Msit-051 <72122896+Dileep-Msit-051@users.noreply.github.com>
2021-08-02 11:50:31 +0530

    1

--- SayHi.java
+++ SayHi.java
@@ -13,10 +13,14 @@ public class SayHi {
         age=i;
     }
     public void Display(){
-        System.out.println("Hi"+","+"my name is"+n+"" )
+        System.out.println("Hi"+","+"my name is"+" "+"name"+" "+"and"+" "+"I am "+"age"+" "+"years old!" );
     }
 
     public static void main(String args[]) {
+        SayHi d1=new SayHi("Dot",4);
+        SayHi d2=new SayHi("ELF",3);
+        d1.Display();
+        d2.Display();
         
     }
 }
